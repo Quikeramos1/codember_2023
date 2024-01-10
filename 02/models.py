8d37980c6043cbcd5a6c78f6262a76050a33a298
Quike Ramos Fuster <quike@MacBook-Pro-de-Quike.local>
2024-01-10 19:24:52 +0100

    challenge_02

--- 02/models.py
+++ 02/models.py
@@ -34,3 +34,8 @@ def formato(solucion):
     return solucion
 
 
+from models import *
+
+file = open_file("/Users/quike/Desktop/codember23/02/message_02.txt")
+solucion =deshide(file)
+print(solucion)
