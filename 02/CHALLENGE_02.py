c25be5d2a56590f709b9f577d14b22df7ae38b76
Quikeramos1 <133090561+Quikeramos1@users.noreply.github.com>
2024-01-26 21:30:49 +0100

    Inicio challenge_04

--- 02/CHALLENGE_02.py
+++
@@ -1,5 +0,0 @@
-from models import *
-
-file = open_file("/Users/quike/Desktop/codember23/02/message_02.txt")
-solucion =deshide(file)
-print(solucion)
--- 03/CHALLENGE_03.py
+++
@@ -1,4 +0,0 @@
-from models import *
-file_path = "/Users/quike/Desktop/codember23/03/encryption_policies.txt"
-file = open_file(file_path)
-print(valid_pass(file))
